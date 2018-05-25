246e76dda96b11d94af9c79737898ef1f2bf6d8c
bajay <bajay@C229>
2018-05-25 15:58:54 +0530

    initial commit

---
+++ TestReactMovieApplication/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestReactMovieApplication
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /* Define any composite PKs here*/
+            modelBuilder.Entity<MovieActor>()
+                .HasKey(m => new { m.MovieId, m.ActorId });
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieActor> MovieActors { get; set; }
+    }
+}
